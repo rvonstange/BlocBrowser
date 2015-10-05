8697216ac54bc2f4ce70fe37e6ff42d902c24410
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-10-05 11:34:40 -0400

    bb activity indicator done

---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Robert von Stange on 10/5/15.
+//  Copyright (c) 2015 Robert von Stange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
